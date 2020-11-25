f1eaf5c3b375c8f9f70d104cb527e8d12dc02b63
Rafael Breno <rafa.b.reis@hotmail.com>
2020-11-25 13:26:37 -0300

    Errors Handlers

--- src/main/java/com/rafael/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rafael/cursomc/dto/CategoriaDTO.java
@@ -2,11 +2,18 @@ package com.rafael.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.rafael.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigratório")
+	@Length(min=5, max=80, message="O Tamanho deve ser entre 5 e 80")
 	private String nome;
 	
 	public CategoriaDTO() {
